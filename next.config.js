2b0901654741794cb60eda63d3b1160bd762edff
matxxtia <matxxtia@hotmail.it>
2025-07-24 14:13:27 +0200

    Add next-pwa and update config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -39,4 +43,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
